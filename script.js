5b2d2861dcbbdb6e1ae01962ce6ff2b218ac591f
AbhinavRoyce <abhinava5p5@gmail.com>
2026-05-15 23:27:20 +0530

    Update

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function typeLoop() {
 
   let speed = 80;
 
-  if (currentCharIndex === currentPhrase.) {
+  if (currentCharIndex === currentPhrase.length) {
     isDeleting = true;
     speed = 1400; // pause at end
   } else if (currentCharIndex < 0) {
