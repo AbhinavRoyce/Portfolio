01f21837f7eb7265a74adc390997cb035bc124f0
AbhinavRoyce <abhinava5p5@gmail.com>
2026-05-15 23:25:44 +0530

    Update

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function typeLoop() {
     typedTextEl.textContent = currentPhrase.slice(0, currentCharIndex--);
   }
 
-  let speed = 80;
+  let speed = ;
 
   if (currentCharIndex === currentPhrase.length) {
     isDeleting = true;
